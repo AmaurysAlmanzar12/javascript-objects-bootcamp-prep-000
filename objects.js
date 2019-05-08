28aa8f7f1f86354298f7d192ecfbfa5c9204d808
Amaurys Almanzar <amaurys.almanzar@wework.com>
2019-05-08 03:31:11 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {artistName: 'songTitle'}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.artistName = 'songTitle'
+  artistName, 'songTitle'
   return playlist
 }
